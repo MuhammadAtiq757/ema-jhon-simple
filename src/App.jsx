7e81e4086b811e5ab6de1a55f24d008906359e65
MuhammadAtiq <muhammadatiq757@gamil.com>
2023-03-28 15:04:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/header/Header'
+import Shop from './components/shop/Shop'
+import Product from './components/product/Product'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+
+      <Header></Header>
+      <Shop></Shop>
+     
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    console.log(props.product);
+
+    const { img, name, seller, ratings, price } = props.product;
+    return (
+        <div className='product'>
+
+            <img src={img} alt="" />
+           <div className='products-info'>
+           <h6 className='product-name'>{name}</h6>
+            <p>Price: {price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Ratings: {ratings} Stars</p>
+           </div>
+          <button className='btn-cart'>Add to cart</button>
+
+        </div>
+    );
+};
+
+export default Product;
